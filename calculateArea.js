232174cc99aa2c704bbfa4099a8c6a697519e1df
mexsov <151562135+mexsov@users.noreply.github.com>
2023-12-20 17:17:37 +0200

    update

--- calculateArea.js
+++ calculateArea.js
@@ -1,3 +1,16 @@
+// 1. Task
+/**
+* Calculates the area of a rectangle.
+* @param {number} length - The length of the rectangle.
+* @param {number} width - The width of the rectangle.
+* @returns {number} The area of the rectangle. before example: 2, 3 after example: 6
+*/
+// Hints to help solve the task:
+// 1. The area of a rectangle is calculated by multiplying its length and width.
+// 2. Create a function that takes the length and width as parameters.
+// 3. Use the formula `area = length * width` to calculate the area.
+// 4. Return the calculated area.
+
 function calculatedArea(length, width){
     const area= length * width
     return area
