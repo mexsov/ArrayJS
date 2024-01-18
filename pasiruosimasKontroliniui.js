f6dbb11b644282b208a5b0c5ed5a2247a20daeb9
mexsov <151562135+mexsov@users.noreply.github.com>
2024-01-18 20:56:12 +0200

    upd

---
+++ pasiruosimasKontroliniui.js
@@ -0,0 +1,30 @@
+// // 1
+
+// let sandelis = {}
+// sandelis = {
+//   staliniaiKompiuteriai:30,
+//   nesiojamiKompiuteriai:35,
+//   isorinesDuomenuLaikmenos:125,
+// }
+// // console.log(sandelis);
+
+// sandelioArchyvas={...sandelis};
+// // console.log(sandelioArchyvas);
+// delete(sandelis.staliniaiKompiuteriai);
+// console.log(sandelis);
+// console.log(sandelioArchyvas);
+
+
+
+
+// 2.
+
+let knyga = {
+    autorius : Abigal Dean,
+    leidejas : Alma littera,
+    metai : 2021,
+    puslapiai : 384, 
+    kalba : Lietuviu,
+    ivertinimas : 4.5/5
+
+}
